99377eb77d1ae03b5041c65c21a72a2c8bfd62d8
Wessias <viggowessner@gmail.com>
2020-11-09 16:18:43 +0100

    Added shiet from earlier lessons

---
+++ HowToNotMakeACalc/Operator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace HowToNotMakeACalc
+{
+    class Operator
+    {
+        public virtual float Operation(float num1, float num2)
+        {
+            var result = 0;
+
+            return result;
+
+
+        }
+    }
+}
